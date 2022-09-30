668d451be604884cdbb0a9cc0ad1c799b768f46c
zeix <z030324@163.com>
2022-09-30 20:25:59 +0800

    finsh springboot

--- zipkin/src/main/java/com/example/ZipkinApplication.java
+++ zipkin/src/main/java/com/example/ZipkinApplication.java
@@ -2,12 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ConfigClientApplication {
+@EnableZipkinServer //声明启动zipkin server
+public class ZipkinApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class,args);
+        SpringApplication.run(ZipkinApplication.class,args);
     }
 }
